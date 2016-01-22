60541ddf83a460a41e45bd989511b184d3331ab9
Mitchell Caisse <computers1055@gmail.com>
2016-01-21 19:56:25 -0500

    Added the new Android project

---
+++ CarTracker.Android/app/src/main/java/com/ricex/cartracker/android/BluetoothDeviceChooserDialogFragment.java
@@ -0,0 +1,55 @@
+package com.ricex.cartracker.android;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.content.DialogInterface;
+import android.os.Bundle;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
+/** Fragmnet to show the dialog to select a bluetooth device
+ *
+ * Created by Mitchell on 1/16/2016.
+ */
+public class BluetoothDeviceChooserDialogFragment extends DialogFragment {
+
+
+    /*
+        Gets a list of all currently paired bluetooth devices
+    */
+    protected List<BluetoothDevice> getAvailaleBluetoohDevices() {
+        BluetoothAdapter adapter = BluetoothAdapter.getDefaultAdapter();
+        Set<BluetoothDevice> pairedDevices = adapter.getBondedDevices();
+        return new ArrayList<BluetoothDevice>(pairedDevices);
+    }
+
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+
+        List<BluetoothDevice> devices = getAvailaleBluetoohDevices();
+
+        List<String> deviceNames = new ArrayList<String>();
+        for (BluetoothDevice device : devices) {
+            deviceNames.add(device.getName());
+        }
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Choose a Bluetooth Device");
+        builder.setItems(deviceNames.toArray(), new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+            }
+        };
+
+    }
+
+
+
+
+}
---
+++ CarTracker.Android/app/src/main/java/com/ricex/cartracker/android/MainActivity.java
@@ -0,0 +1,89 @@
+package com.ricex.cartracker.android;
+
+import android.app.AlertDialog;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    protected static final int REQUEST_ENABLE_BT = 5598;
+
+    protected boolean triedEnableBluetooth;
+
+    public MainActivity() {
+        triedEnableBluetooth = false;
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    protected void onResume(Bundle savedInstanceState) {
+
+        //check for bluetooth enabled
+        if (!isBluetoothSupported()) {
+            //device does not support bluetooth, inform user and then exit
+            //or just exit for now.
+            finish();
+        }
+
+
+        if  (!isBluetoothEnabled()) {
+            //check if we tried to enable bluetooth
+            if (triedEnableBluetooth) {
+                //we did, so it obviously failed, inform the user that application need bluetooth and exit
+                finish();
+            }
+            else {
+                promptUserEnableBluetooth();
+            }
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_ENABLE_BT) {
+            triedEnableBluetooth = true;
+        }
+    }
+
+
+    /**
+     *  Checks if bluetooth is supported on this device or not
+     * @return True if bluetooth is supported, false otherwise
+     */
+    protected boolean isBluetoothSupported() {
+        BluetoothAdapter adapter = BluetoothAdapter.getDefaultAdapter();
+        return null != adapter;
+    }
+
+    /**
+     *  Checks if Bluetooth is currently enabled on this device or not
+     * @return True if bluetooth is enabled, false otehrwise
+     */
+    protected boolean isBluetoothEnabled() {
+        BluetoothAdapter adapter = BluetoothAdapter.getDefaultAdapter();
+        return adapter.isEnabled();
+    }
+
+    /**
+     *  Creates the intent to prompt the user to enable bluetooth
+     */
+    protected void promptUserEnableBluetooth() {
+        Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+        startActivityForResult(enableBtIntent, REQ
+    }
+
+}
