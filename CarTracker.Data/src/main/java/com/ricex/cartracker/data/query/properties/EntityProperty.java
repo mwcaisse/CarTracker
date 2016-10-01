193e9c600ea2507b647d67be7c6939308ac49346
Mitchell Caisse <computers1055@gmail.com>
2016-10-01 13:06:24 -0400

    Added property enums for the entities

---
+++ CarTracker.Data/src/main/java/com/ricex/cartracker/data/query/properties/EntityProperty.java
@@ -0,0 +1,7 @@
+package com.ricex.cartracker.data.query.properties;
+
+public interface EntityProperty {
+
+	public String getPropertyField();
+	
+}
---
+++ CarTracker.Data/src/main/java/com/ricex/cartracker/data/query/properties/Properties.java
@@ -0,0 +1,117 @@
+package com.ricex.cartracker.data.query.properties;
+
+public class Properties {
+
+	public enum Car implements EntityProperty {
+		
+		ID,
+		VIN,
+		NAME,
+		OWNER_ID,
+		CREATE_DATE,
+		MODIFIED_DATE;
+		
+		private final String propertyField;
+		
+		private Car() {
+			this.propertyField = this.name();
+		}
+		
+		private Car(String propertyField) {
+			this.propertyField = propertyField;
+		}
+
+		public String getPropertyField() {
+			return propertyField;
+		}		
+	}
+	
+	public enum Reading implements EntityProperty {
+		
+		ID,
+		READ_DATE,
+		TRIP_ID,
+		LATITUDE,
+		LONGITUDE,
+		AIR_INTAKE_TEMP,
+		AMBIENT_AIR_TEMP,
+		RENGINE_COOLANT_TEMP,
+		OIL_TEMP,
+		ENGINE_RPM,
+		SPEED,
+		MASS_AIR_FLOW,
+		THROTTLE_POSITION,
+		FUEL_TYPE,
+		FUEL_LEVEL,
+		CREATE_DATE,
+		MODIFIED_DATE;
+		
+		private final String propertyField;
+		
+		private Reading() {
+			this.propertyField = this.name();
+		}
+		
+		private Reading(String propertyField) {
+			this.propertyField = propertyField;
+		}
+
+		public String getPropertyField() {
+			return propertyField;
+		}		
+	}
+	
+	public enum Trip implements EntityProperty {
+		
+		ID,
+		START_DATE,
+		END_DATE,
+		NAME,
+		CAR_ID,
+		CREATE_DATE,
+		MODIFIED_DATE;
+		
+		private final String propertyField;
+		
+		private Trip() {
+			this.propertyField = this.name();
+		}
+		
+		private Trip(String propertyField) {
+			this.propertyField = propertyField;
+		}
+
+		public String getPropertyField() {
+			return propertyField;
+		}
+	}
+	
+	public enum User implements EntityProperty {
+		
+		ID,
+		USERNAME,
+		PASSWORD,
+		USER_EMAIL,
+		ACTIVE,
+		LOCKED,
+		EXPIRATION_DATE,
+		PASSWORD_EXPIRATION_DATE,
+		CREATE_DATE,
+		MODIFIED_DATE;
+		
+		private final String propertyField;
+		
+		private User() {
+			this.propertyField = this.name();
+		}
+		
+		private User(String propertyField) {
+			this.propertyField = propertyField;
+		}
+
+		public String getPropertyField() {
+			return propertyField;
+		}
+	}
+	
+}
