89f6af8a3d33ef2e86bc012d7a5806bac0876de7
Mitchell Caisse <computers1055@gmail.com>
2016-10-05 22:10:09 -0400

    Basic and Hacky map implementation, working

--- CarTracker.Web/src/main/webapp/app/Modules/Trip/TripMap/TripMap.js
+++ CarTracker.Web/src/main/webapp/app/Modules/Trip/TripMap/TripMap.js
@@ -15,8 +15,62 @@ define("Modules/Trip/TripMap/TripMap", ["AMD/googlemaps!", "moment", "Service/sy
 
 		self.tripId = opts.tripId;
 		
-		self.load = function() {
+		$(document).ready(function () {
 			
+			self.map = new gmaps.Map(document.getElementById("tripMap"), {
+				zoom: 12,
+				center: {lat: 42.710291, lng: -71.442039}		        
+			});
+			
+			proxy.reading.getAllForTrip(self.tripId).then(function (data) {
+				var coords = $.map(data, function (elm, ind) {
+					if (elm.longitude === 0 && elm.latitude === 0) {
+						return null; //ignore any coordinates that have 0 as latitude and longitude (data issue)
+					}
+					return {
+						lat: elm.latitude,
+						lng: elm.longitude
+					};
+				});	
+				
+				var routePath = new gmaps.Polyline({
+					path: coords,
+					geodesic: true,
+					strokeColor: "#FF0000",
+					strokeOpacity: 1.0,
+					strokeWeight: 2.0
+				});
+				
+				routePath.setMap(self.map);
+				
+			});
+		});
+		
+		self.load = function() {
+			/*
+			proxy.reading.getAllForTrip(self.tripId).then(function (data) {
+				var coords = $.map(data, function (elm, ind) {
+					if (elm.longitude === 0 && elm.latitude === 0) {
+						return; //ignore any coordinates that have 0 as latitude and longitude (data issue)
+					}
+					return {
+						lat: elm.latitude,
+						lng: elm.longitude
+					};
+				});	
+				
+				var routePath = new gmaps.Polyline({
+					path: coords,
+					geodesic: true,
+					strokeColor: "#FF0000",
+					strokeOpacity: 1.0,
+					strokeWeight: 2.0
+				});
+				
+				routePath.setMap(self.Map);
+				
+			});*/
+			                                                                
 		};	
 		
 		self.refresh = function () {
