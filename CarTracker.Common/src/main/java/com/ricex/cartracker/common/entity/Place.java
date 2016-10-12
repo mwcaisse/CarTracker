ebc2ac46fb2703a434f96c10e74425ef682f2436
Mitchell Caisse <computers1055@gmail.com>
2016-10-11 22:04:55 -0400

    Added Place entity, table, and mapper

---
+++ CarTracker.Common/src/main/java/com/ricex/cartracker/common/entity/Place.java
@@ -0,0 +1,69 @@
+package com.ricex.cartracker.common.entity;
+
+public class Place extends AbstractEntity {
+
+	private String name;
+	
+	private double latitude;
+	
+	private double longitude;
+	
+	private boolean active;
+
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+
+	/**
+	 * @param name the name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	/**
+	 * @return the latitude
+	 */
+	public double getLatitude() {
+		return latitude;
+	}
+
+	/**
+	 * @param latitude the latitude to set
+	 */
+	public void setLatitude(double latitude) {
+		this.latitude = latitude;
+	}
+
+	/**
+	 * @return the longitude
+	 */
+	public double getLongitude() {
+		return longitude;
+	}
+
+	/**
+	 * @param longitude the longitude to set
+	 */
+	public void setLongitude(double longitude) {
+		this.longitude = longitude;
+	}
+
+	/**
+	 * @return the active
+	 */
+	public boolean isActive() {
+		return active;
+	}
+
+	/**
+	 * @param active the active to set
+	 */
+	public void setActive(boolean active) {
+		this.active = active;
+	}	
+	
+}
---
+++ CarTracker.Data/src/main/java/com/ricex/cartracker/data/mapper/PlaceMapper.java
@@ -0,0 +1,7 @@
+package com.ricex.cartracker.data.mapper;
+
+import com.ricex.cartracker.common.entity.Place;
+
+public interface PlaceMapper extends EntityMapper<Place> {
+
+}
