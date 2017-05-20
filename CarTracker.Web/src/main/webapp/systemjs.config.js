5c299a9cc96a4a396d7768f0d2f78aaa6e0ad307
Mitchell Caisse <computers1055@gmail.com>
2017-05-19 23:08:12 -0400

    Got angular working

--- CarTracker.Web/src/main/webapp/systemjs.config.js
+++ CarTracker.Web/src/main/webapp/systemjs.config.js
@@ -6,12 +6,12 @@
     System.config({
         paths: {
             // paths serve as alias
-            'npm:': 'lib/'
+            'npm:': '/cartracker/lib/'
         },
         // map tells the System loader where to look for things
         map: {
             // our app is within the app folder
-            app: 'app',
+            app: '/cartracker/app',
             // angular bundles
             '@angular/core': 'npm:@angular/core/bundles/core.umd.js',
             '@angular/common': 'npm:@angular/common/bundles/common.umd.js',
