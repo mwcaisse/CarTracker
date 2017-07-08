ad934651f8fe608309b55d5d5d7ba0614e3a9b32
Mitchell Caisse <computers1055@gmail.com>
2017-07-07 23:22:11 -0400

    Added the content padding to layout instead of singular page. Updated navigation to only show nav links if authenticated

--- CarTracker.Web/src/main/webapp/app/Views/Navigation/Navigation.js
+++ CarTracker.Web/src/main/webapp/app/Views/Navigation/Navigation.js
@@ -5,6 +5,9 @@ define("Views/Navigation/Navigation",
 		 "AMD/text!Views/Navigation/Navigation.html"], 
 		
 		function(navigation, navLink, template) {
+			
+			var isAuthenticated = $("#isAuthenticated").val() === "true";
+	
 			var vm = function (elementId) {
 				return new Vue({			
 					el: elementId,
@@ -15,36 +18,39 @@ define("Views/Navigation/Navigation",
 					},
 					methods: {
 						initalizeLinks: function () {
-							this.navigationLinks.push({
-								id: "Home", name: "Home", link: navigation.homeLink()
-							});
-							this.navigationLinks.push({
-								id: "Car", name: "Car", link: navigation.carsLink()
-							});
-							this.navigationLinks.push({
-								id: "Trip", name: "Trip", link: navigation.tripsLink()
-							});
-							
-							//Create the log links
-							var logLink = { id: "Log", name: "Log", link: "#", subLinks: [] };
-							logLink.subLinks.push({
-								id: "Log/Reader", name: "Reader", link: navigation.readerLogLink()
-							});
-							
-							this.navigationLinks.push(logLink);
-							
-							var userNav = {
-								id: "User", name: "User", link: "#", subLinks: []
-							};
-							
-							userNav.subLinks.push({
-								id: "User/AuthTokens", name: "Tokens", link: navigation.userAuthenticationTokensLink()
-							});
-							userNav.subLinks.push({
-								id: "User/Logout", name: "Logout", link: navigation.logoutLink()
-							});
-							
-							this.rightNavigationLinks.push(userNav);
+							//we only add navigation links if we are authenticated
+							if (isAuthenticated) {
+								this.navigationLinks.push({
+									id: "Home", name: "Home", link: navigation.homeLink()
+								});
+								this.navigationLinks.push({
+									id: "Car", name: "Car", link: navigation.carsLink()
+								});
+								this.navigationLinks.push({
+									id: "Trip", name: "Trip", link: navigation.tripsLink()
+								});
+								
+								//Create the log links
+								var logLink = { id: "Log", name: "Log", link: "#", subLinks: [] };
+								logLink.subLinks.push({
+									id: "Log/Reader", name: "Reader", link: navigation.readerLogLink()
+								});
+								
+								this.navigationLinks.push(logLink);
+								
+								var userNav = {
+									id: "User", name: "User", link: "#", subLinks: []
+								};
+								
+								userNav.subLinks.push({
+									id: "User/AuthTokens", name: "Tokens", link: navigation.userAuthenticationTokensLink()
+								});
+								userNav.subLinks.push({
+									id: "User/Logout", name: "Logout", link: navigation.logoutLink()
+								});
+								
+								this.rightNavigationLinks.push(userNav);
+							}
 						}
 					},
 					created: function () {
