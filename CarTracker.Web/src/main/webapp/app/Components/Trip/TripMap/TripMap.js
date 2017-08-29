dbf6f15e43cd54a1c8bda17682d74775a224c8b8
Mitchell Caisse <computers1055@gmail.com>
2017-08-28 22:11:07 -0400

    added different coloring depending on speed

--- CarTracker.Web/src/main/webapp/app/Components/Trip/TripMap/TripMap.js
+++ CarTracker.Web/src/main/webapp/app/Components/Trip/TripMap/TripMap.js
@@ -10,7 +10,7 @@ define("Components/Trip/TripMap/TripMap",
 		data: function() {
 			return {	
 				map: {},
-				routePath: null
+				routePathes: []
 			}
 		},	
 		props: {
@@ -28,8 +28,10 @@ define("Components/Trip/TripMap/TripMap",
 				proxy.reading.getAllForTrip(this.tripId).then(function (data) {
 					
 					//if we have an existing route path remove it from the map
-					if (this.routePath) {
-						this.routePath.setMap(null);
+					if (this.routePathes.length > 0) {
+						$.each(this.routePathes, function (elm, ind) {
+							elm.setMap(null);
+						});
 					}
 					
 					var coords = $.map(data, function (elm, ind) {
@@ -37,31 +39,49 @@ define("Components/Trip/TripMap/TripMap",
 							return null; // ignore any coordinates that have 0 as lat an long (data issue)
 						}
 						return {
-							lat: elm.latitude,
-							lng: elm.longitude
-						}
+							speed: elm.speed,						
+							coords: {
+								lat: elm.latitude,
+								lng: elm.longitude
+							}
+						};
 					});
 					
 					var mapBounds = new gmaps.LatLngBounds();
 					$.each(coords, function (ind, elm) {
-						mapBounds.extend(elm);
+						mapBounds.extend(elm.coords);
 					});
 					
-					this.routePath = new gmaps.Polyline({
-						path: coords,
-						geodesic: true,
-						strokeColor: "#FF0000",
-						strokeOpacity: 1.0,
-						strokeWeight: 2.0
-					});
-					
-					this.routePath.setMap(this.map);
+					for (var i = 0; i < coords.length - 1; i ++) {
+						var color = "";
+						
+						if (coords[i].speed < 60) {
+							color = "#FF0000";
+						}
+						else if (coords[i].speed < 100) {
+							color = "#E59400"
+						}
+						else {
+							color = "#00FF00";
+						}						
+						var subPath = new gmaps.Polyline({
+							path: [coords[i].coords, coords[i+1].coords],
+							geodesic: true,
+							strokeColor: color,
+							strokeOpacity: 1.0,
+							strokeWeight: 2.0
+						});
+						
+						subPath.setMap(this.map);
+						
+						this.routePathes.push(subPath);
+					};
 					
 					this.map.fitBounds(mapBounds);
 					
 					if (coords.length > 1) {				
 						var endMarker = new gmaps.Marker({
-							position: coords[coords.length - 1],
+							position: coords[coords.length - 1].coords,
 							map: this.map,
 							title: "End"
 						});
